e355eae0ccc006ef3c1904850a2ead06ec57bc09
Noah <zap.noah@gmail.com>
2022-11-23 19:37:51 -0500

    add development core

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const { transporter, verify } = require('./utils/mailer');
 
 const port = process.env.PORT || 8080;
 const app = express();
-const NODE_ENV = process.env.NODE_ENV;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.listen(port, () => {
   expressConfig(app);
